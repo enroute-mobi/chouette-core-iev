61e1c77e55204179bb1ce609964bc73ea8ef91e6
flabourot <flabourot@cityway.fr>
2017-02-14 16:26:47 +0100

    Beginning of NetexStructureParser

--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/NetexStructureParser.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/NetexStructureParser.java
@@ -1,13 +1,45 @@
 package mobi.chouette.exchange.netex_stif.parser;
 
+import org.xmlpull.v1.XmlPullParser;
+
+import lombok.extern.log4j.Log4j;
 import mobi.chouette.common.Context;
 import mobi.chouette.exchange.importer.Parser;
+import mobi.chouette.exchange.importer.ParserFactory;
+import mobi.chouette.exchange.netex_stif.Constant;
 
-public class NetexStructureParser implements Parser {
+@Log4j
+public class NetexStructureParser implements Parser, Constant {
 
 	@Override
 	public void parse(Context context) throws Exception {
-		
+		XmlPullParser xpp = (XmlPullParser) context.get(PARSER);
+		while (xpp.nextTag() == XmlPullParser.START_TAG) {
+			if (xpp.getName().equals(TYPE_OF_FRAME_REF)) {
+				if (xpp.nextText().equals(NETEX_STRUCTURE)){
+					// TODO dire que c'est valide ou à l'inverse que c'est invalide
+				}
+			}else if (xpp.getName().equals(MEMBERS)) {
+				while (xpp.nextTag() == XmlPullParser.START_TAG) {
+					if (xpp.getName().equals(ROUTES)) {
+						while (xpp.nextTag() == XmlPullParser.START_TAG) {
+							if (xpp.getName().equals(ROUTE)) {
+								Parser routeParser= ParserFactory.create(RouteParser.class.getName());
+								routeParser.parse(context);
+							}
+						}
+						
+					}else if (xpp.getName().equals(DIRECTIONS))  {
+						while (xpp.nextTag() == XmlPullParser.START_TAG) {
+							if (xpp.getName().equals(DIRECTION)) {
+								Parser directionParser= ParserFactory.create(DirectionParser.class.getName());
+								directionParser.parse(context);
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
