aa2f9b8096416024f87260746ebfaeb777d2a153
flabourot <flabourot@cityway.fr>
2017-02-14 16:26:22 +0100

    Implementation of DirectionParser

--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/DirectionParser.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/DirectionParser.java
@@ -1,14 +1,35 @@
 package mobi.chouette.exchange.netex_stif.parser;
 
-import mobi.chouette.common.Context;
-import mobi.chouette.exchange.importer.Parser;
+import org.xmlpull.v1.XmlPullParser;
 
-public class DirectionParser implements Parser {
+import lombok.extern.log4j.Log4j;
+import mobi.chouette.common.Context;
+import mobi.chouette.common.XPPUtil;
+import mobi.chouette.exchange.importer.Parser;
+import mobi.chouette.exchange.netex_stif.Constant;
+import mobi.chouette.exchange.netex_stif.model.Direction;
+import mobi.chouette.exchange.netex_stif.model.NetexStifObjectFactory;
+
+@Log4j
+public class DirectionParser implements Parser, Constant {
 
 	@Override
 	public void parse(Context context) throws Exception {
-		// TODO Auto-generated method stub
-
+		XmlPullParser xpp = (XmlPullParser) context.get(PARSER);
+		NetexStifObjectFactory factory = (NetexStifObjectFactory)context.get(NETEX_STIF_OBJECT_FACTORY);
+		
+		Integer version = (Integer) context.get(VERSION);
+		String id = xpp.getAttributeValue(null, ID);
+		Direction direction = factory.getDirection(id);
+		direction.setObjectVersion(version);
+		while (xpp.nextTag() == XmlPullParser.START_TAG) {
+			if (xpp.getName().equals(NAME)) {
+				direction.setName(xpp.nextText());
+			}
+			else{
+				XPPUtil.skipSubTree(log, xpp);
+			}
+		}
 	}
 
 }
