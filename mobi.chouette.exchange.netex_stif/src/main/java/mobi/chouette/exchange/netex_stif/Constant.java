1096ddbd06623d53ff62ffe867aba0f461a38548
flabourot <flabourot@cityway.fr>
2017-02-15 11:12:14 +0100

    Implementing StopPointInJourneyPattern

--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/Constant.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/Constant.java
@@ -4,6 +4,7 @@ public interface Constant extends mobi.chouette.common.Constant {
 
 	public static final String ID = "id";
 	public static final String VERSION = "version";
+	public static final String ORDER = "order";
 	public static final String REF = "ref";
 	public static final String NAME = "Name";
 	public static final String DISTANCE = "Distance";
--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/StopPointInJourneyPatternParser.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/StopPointInJourneyPatternParser.java
@@ -2,19 +2,33 @@ package mobi.chouette.exchange.netex_stif.parser;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import lombok.extern.log4j.Log4j;
 import mobi.chouette.common.Context;
+import mobi.chouette.common.XPPUtil;
 import mobi.chouette.exchange.importer.Parser;
 import mobi.chouette.exchange.netex_stif.Constant;
+import mobi.chouette.model.StopPoint;
 import mobi.chouette.model.util.ObjectFactory;
 import mobi.chouette.model.util.Referential;
 
+@Log4j
 public class StopPointInJourneyPatternParser implements Parser, Constant {
 
 	@Override
 	public void parse(Context context) throws Exception {
 		XmlPullParser xpp = (XmlPullParser) context.get(PARSER);
 		Referential referential = (Referential) context.get(REFERENTIAL);
-		//ObjectFactory.getStopPoint(referential, objectId)
+
+		Integer version = (Integer) context.get(VERSION);
+
+		// xpp.require(XmlPullParser.START_TAG, null, ROUTE);
+		String id = xpp.getAttributeValue(null, ID);
+		String order = xpp.getAttributeValue(null, ORDER);
+		String objectId = id + ":" + order;
+		StopPoint stopPoint = ObjectFactory.getStopPoint(referential, objectId);
+		stopPoint.setObjectVersion(version);
+		XPPUtil.skipSubTree(log, xpp);
+
 	}
 
 }
