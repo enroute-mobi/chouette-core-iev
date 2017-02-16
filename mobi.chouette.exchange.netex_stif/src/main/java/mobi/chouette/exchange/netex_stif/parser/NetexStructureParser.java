44932d8d2aea5a3cc86a1c7ced895e1fdaa2344f
flabourot <flabourot@cityway.fr>
2017-02-16 11:02:24 +0100

    refactor if then else to map

--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/NetexStructureParser.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/NetexStructureParser.java
@@ -1,5 +1,8 @@
 package mobi.chouette.exchange.netex_stif.parser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.xmlpull.v1.XmlPullParser;
 
 import lombok.extern.log4j.Log4j;
@@ -8,6 +11,7 @@ import mobi.chouette.common.XPPUtil;
 import mobi.chouette.exchange.importer.Parser;
 import mobi.chouette.exchange.importer.ParserFactory;
 import mobi.chouette.exchange.netex_stif.Constant;
+import mobi.chouette.exchange.netex_stif.model.DestinationDisplay;
 
 @Log4j
 public class NetexStructureParser implements Parser, Constant {
@@ -18,69 +22,59 @@ public class NetexStructureParser implements Parser, Constant {
 	public void parse(Context context) throws Exception {
 		XmlPullParser xpp = (XmlPullParser) context.get(PARSER);
 		while (xpp.nextTag() == XmlPullParser.START_TAG) {
-			while (xpp.nextTag() == XmlPullParser.START_TAG) {
-				if (xpp.getName().equals(ROUTES)) {
-					while (xpp.nextTag() == XmlPullParser.START_TAG) {
-						if (xpp.getName().equals(ROUTE)) {
-							Parser routeParser = ParserFactory.create(RouteParser.class.getName());
-							routeParser.parse(context);
-						}
-					}
-				} else if (xpp.getName().equals(DIRECTIONS)) {
-					while (xpp.nextTag() == XmlPullParser.START_TAG) {
-						if (xpp.getName().equals(DIRECTION)) {
-							Parser directionParser = ParserFactory.create(DirectionParser.class.getName());
-							directionParser.parse(context);
-						}
-					}
-				} else if (xpp.getName().equals(SERVICE_JOURNEY_PATTERNS)) {
-					while (xpp.nextTag() == XmlPullParser.START_TAG) {
-						if (xpp.getName().equals(SERVICE_JOURNEY_PATTERN)) {
-							Parser serviceJourneyPatternParser = ParserFactory
-									.create(ServiceJourneyPatternParser.class.getName());
-							serviceJourneyPatternParser.parse(context);
-						}
-					}
-				} else if (xpp.getName().equals(DESTINATION_DISPLAYS)) {
-					while (xpp.nextTag() == XmlPullParser.START_TAG) {
-						if (xpp.getName().equals(DESTINATION_DISPLAY)) {
-							Parser destinationDisplayParser = ParserFactory
-									.create(DestinationDisplayParser.class.getName());
-							destinationDisplayParser.parse(context);
-						}
-					}
-				} else if (xpp.getName().equals(SCHEDULED_STOP_POINTS)) {
-					while (xpp.nextTag() == XmlPullParser.START_TAG) {
-						if (xpp.getName().equals(SCHEDULED_STOP_POINT)) {
-							Parser scheduledStopPointParser = ParserFactory
-									.create(ScheduledStopPointParser.class.getName());
-							scheduledStopPointParser.parse(context);
-						}
-					}
-				} else if (xpp.getName().equals(PASSENGER_STOP_ASSIGNEMENTS)) {
-					while (xpp.nextTag() == XmlPullParser.START_TAG) {
-						if (xpp.getName().equals(PASSENGER_STOP_ASSIGNEMENT)) {
-							Parser passengerStopAssignementParser = ParserFactory
-									.create(PassengerStopAssignementParser.class.getName());
-							passengerStopAssignementParser.parse(context);
-						}
-					}
-				} else if (xpp.getName().equals(ROUTING_CONSTRAINT_ZONES)) {
-					while (xpp.nextTag() == XmlPullParser.START_TAG) {
-						if (xpp.getName().equals(ROUTING_CONSTRAINT_ZONE)) {
-							Parser routingConstraintZoneParser = ParserFactory
-									.create(RoutingConstraintZoneParser.class.getName());
-							routingConstraintZoneParser.parse(context);
-						}
-					}
-				} else {
-					XPPUtil.skipSubTree(log, xpp);
-				}
+			String name = xpp.getName();
+			// check if it is one of the member we treat
+			if (members.containsKey(name)) {
+				parseMember(name, xpp, context);
+			} else {
+				XPPUtil.skipSubTree(log, xpp);
 			}
 		}
 	}
 
+	private void parseMember(String tag, XmlPullParser xpp, Context context) throws Exception {
+		String elt = members.get(tag);
+		if (elt != null) {
+			if (xpp.getName().equals(tag)) {
+				while (xpp.nextTag() == XmlPullParser.START_TAG) {
+					if (xpp.getName().equals(elt)) {
+						String clazz = parsers.get(elt);
+						if (clazz != null) {
+							Parser parser = ParserFactory.create(clazz);
+							parser.parse(context);
+						} else {
+							XPPUtil.skipSubTree(log, xpp);
+						}
+					}
+				}
+			}
+		} else {
+			XPPUtil.skipSubTree(log, xpp);
+		}
+	}
+
+	static Map<String, String> members = new HashMap<>();
+	static Map<String, String> parsers = new HashMap<>();
+
 	static {
+		// init used members
+		members.put(ROUTES, ROUTE);
+		members.put(DIRECTIONS, DIRECTION);
+		members.put(SERVICE_JOURNEY_PATTERNS, SERVICE_JOURNEY_PATTERN);
+		members.put(DESTINATION_DISPLAYS, DESTINATION_DISPLAY);
+		members.put(SCHEDULED_STOP_POINTS, SCHEDULED_STOP_POINT);
+		members.put(PASSENGER_STOP_ASSIGNEMENTS, PASSENGER_STOP_ASSIGNEMENT);
+		members.put(ROUTING_CONSTRAINT_ZONES, ROUTING_CONSTRAINT_ZONE);
+
+		// init used parsers
+		parsers.put(ROUTE, RouteParser.class.getName());
+		parsers.put(DIRECTION, DirectionParser.class.getName());
+		parsers.put(SERVICE_JOURNEY_PATTERN, ServiceJourneyPatternParser.class.getName());
+		parsers.put(DESTINATION_DISPLAY, DestinationDisplay.class.getName());
+		parsers.put(SCHEDULED_STOP_POINT, ScheduledStopPointParser.class.getName());
+		parsers.put(PASSENGER_STOP_ASSIGNEMENT, PassengerStopAssignementParser.class.getName());
+		parsers.put(ROUTING_CONSTRAINT_ZONE, RoutingConstraintZoneParser.class.getName());
+
 		ParserFactory.register(NetexStructureParser.class.getName(), new ParserFactory() {
 			private NetexStructureParser instance = new NetexStructureParser();
 
