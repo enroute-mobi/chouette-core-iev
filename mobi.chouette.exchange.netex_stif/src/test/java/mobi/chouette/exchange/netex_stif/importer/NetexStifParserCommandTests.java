7a5e007f42ec3eaf8cb91968e92839c25409a865
flabourot <flabourot@cityway.fr>
2017-02-20 15:29:39 +0100

    add test for commun.xml

--- mobi.chouette.exchange.netex_stif/src/test/java/mobi/chouette/exchange/netex_stif/importer/NetexStifParserCommandTests.java
+++ mobi.chouette.exchange.netex_stif/src/test/java/mobi/chouette/exchange/netex_stif/importer/NetexStifParserCommandTests.java
@@ -9,15 +9,19 @@ import javax.naming.NamingException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.BasicConfigurator;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import mobi.chouette.common.Context;
 import mobi.chouette.common.chain.CommandFactory;
 import mobi.chouette.exchange.netex_stif.Constant;
 import mobi.chouette.exchange.netex_stif.JobDataTest;
+import mobi.chouette.exchange.netex_stif.model.NetexStifObjectFactory;
 import mobi.chouette.exchange.report.ActionReport;
 import mobi.chouette.exchange.report.ReportConstant;
 import mobi.chouette.exchange.validation.report.ValidationReport;
+import mobi.chouette.model.Footnote;
+import mobi.chouette.model.util.ObjectFactory;
 import mobi.chouette.model.util.Referential;
 import mobi.chouette.persistence.hibernate.ContextHolder;
 
@@ -52,6 +56,7 @@ public class NetexStifParserCommandTests implements Constant, ReportConstant {
 		NetexStifImportParameters configuration = new NetexStifImportParameters();
 		context.put(CONFIGURATION, configuration);
 		context.put(REFERENTIAL, new Referential());
+		context.put(NETEX_STIF_OBJECT_FACTORY, new NetexStifObjectFactory());
 		configuration.setName("name");
 		configuration.setUserName("userName");
 		configuration.setNoSave(true);
@@ -100,11 +105,22 @@ public class NetexStifParserCommandTests implements Constant, ReportConstant {
 		File f = new File(path, "commun.xml");
 		parser.setFileURL("file://" + f.getAbsolutePath());
 		parser.execute(context);
-
-		/// Assert.assertXXX();
+		NetexStifObjectFactory factory = (NetexStifObjectFactory) context.get(NETEX_STIF_OBJECT_FACTORY);
+		
+		assertNotice(factory, "CITYWAY:Notice:1:LOC", "Notice 1", "1");
+		assertNotice(factory, "CITYWAY:Notice:2:LOC", "Notice 2", "2");
+		assertNotice(factory, "CITYWAY:Notice:3:LOC", "Notice 3", "3");
+		
+	}
+	
+	private void assertNotice (NetexStifObjectFactory nsof, String id, String text, String publicCode){
+		Footnote fn = nsof.getFootnote(id);
+		Assert.assertEquals(fn.getLabel(), text);
+		Assert.assertEquals(fn.getCode(), publicCode);
+			
 	}
 
-	@Test(groups = { "Nominal" }, description = "commun")
+	@Test(groups = { "Nominal" }, description = "calendrier")
 	public void verifiyCalendrierParser() throws Exception {
 		Context context = initImportContext();
 
