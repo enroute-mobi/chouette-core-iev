5c03f3e5fe5d949fd8bc6284a08dc59e1b154f21
Michel Etienne <metienne@cityway.fr>
2017-03-27 16:25:56 +0200

    reactive hibernate spatial on tests

--- mobi.chouette.exchange.netex_stif/src/test/java/mobi/chouette/exchange/netex_stif/importer/NetexStifLoadSharedDataTests.java
+++ mobi.chouette.exchange.netex_stif/src/test/java/mobi/chouette/exchange/netex_stif/importer/NetexStifLoadSharedDataTests.java
@@ -47,12 +47,11 @@ import mobi.chouette.persistence.hibernate.ContextHolder;
 @Log4j
 public class NetexStifLoadSharedDataTests extends Arquillian implements Constant, ReportConstant {
 
-	@EJB 
+	@EJB
 	LineDAO lineDao;
 
-	@EJB 
+	@EJB
 	VehicleJourneyDAO vjDao;
-
 
 	@PersistenceContext(unitName = "referential")
 	EntityManager em;
@@ -69,59 +68,43 @@ public class NetexStifLoadSharedDataTests extends Arquillian implements Constant
 		List<File> jars = new ArrayList<>();
 		List<JavaArchive> modules = new ArrayList<>();
 		for (File file : files) {
-			if (file.getName().startsWith("mobi.chouette.exchange"))
-			{
-				String name = file.getName().split("\\-")[0]+".jar";
-				JavaArchive archive = ShrinkWrap
-						  .create(ZipImporter.class, name)
-						  .importFrom(file)
-						  .as(JavaArchive.class);
+			if (file.getName().startsWith("mobi.chouette.exchange")) {
+				String name = file.getName().split("\\-")[0] + ".jar";
+				JavaArchive archive = ShrinkWrap.create(ZipImporter.class, name).importFrom(file).as(JavaArchive.class);
 				modules.add(archive);
-			}
-			else
-			{
+			} else {
 				jars.add(file);
 			}
 		}
-		File[] filesDao = Maven.resolver().loadPomFromFile("pom.xml")
-				.resolve("mobi.chouette:mobi.chouette.dao").withTransitivity().asFile();
-		if (filesDao.length == 0) 
-		{
+		File[] filesDao = Maven.resolver().loadPomFromFile("pom.xml").resolve("mobi.chouette:mobi.chouette.dao")
+				.withTransitivity().asFile();
+		if (filesDao.length == 0) {
 			throw new NullPointerException("no dao");
 		}
 		for (File file : filesDao) {
-			if (file.getName().startsWith("mobi.chouette.dao"))
-			{
-				String name = file.getName().split("\\-")[0]+".jar";
-				
-				JavaArchive archive = ShrinkWrap
-						  .create(ZipImporter.class, name)
-						  .importFrom(file)
-						  .as(JavaArchive.class);
+			if (file.getName().startsWith("mobi.chouette.dao")) {
+				String name = file.getName().split("\\-")[0] + ".jar";
+
+				JavaArchive archive = ShrinkWrap.create(ZipImporter.class, name).importFrom(file).as(JavaArchive.class);
 				modules.add(archive);
 				if (!modules.contains(archive))
-				   modules.add(archive);
-			}
-			else
-			{
+					modules.add(archive);
+			} else {
 				if (!jars.contains(file))
-				   jars.add(file);
+					jars.add(file);
 			}
 		}
-		final WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war").addAsWebInfResource("postgres-ds.xml")
-				.addClass(NetexStifLoadSharedDataTests.class)
+		final WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war")
+				.addAsWebInfResource("postgres-ds.xml").addClass(NetexStifLoadSharedDataTests.class)
 				.addClass(JobDataTest.class);
-		
-		result = ShrinkWrap.create(EnterpriseArchive.class, "test.ear")
-				.addAsLibraries(jars.toArray(new File[0]))
-				.addAsModules(modules.toArray(new JavaArchive[0]))
-				.addAsModule(testWar)
+
+		result = ShrinkWrap.create(EnterpriseArchive.class, "test.ear").addAsLibraries(jars.toArray(new File[0]))
+				.addAsModules(modules.toArray(new JavaArchive[0])).addAsModule(testWar)
 				.addAsResource(EmptyAsset.INSTANCE, "beans.xml");
 		return result;
 	}
 
 	protected static InitialContext initialContext;
-	
 
 	protected void init() {
 		Locale.setDefault(Locale.ENGLISH);
@@ -131,10 +114,10 @@ public class NetexStifLoadSharedDataTests extends Arquillian implements Constant
 			} catch (NamingException e) {
 				e.printStackTrace();
 			}
-			
-			
+
 		}
 	}
+
 	protected Context initImportContext() {
 		init();
 		ContextHolder.setContext("chouette_gui"); // set tenant schema
@@ -153,10 +136,10 @@ public class NetexStifLoadSharedDataTests extends Arquillian implements Constant
 		configuration.setReferentialName("test");
 		configuration.setLineReferentialId(1L);
 		configuration.setStopAreaReferentialId(1L);
-		List<Long> ids = Arrays.asList(new Long[]{1L});
+		List<Long> ids = Arrays.asList(new Long[] { 1L, 2L });
 		configuration.setIds(ids);
 		JobDataTest jobData = new JobDataTest();
-		context.put(JOB_DATA,jobData);
+		context.put(JOB_DATA, jobData);
 		jobData.setPathName("target/referential/test");
 		File f = new File("target/referential/test");
 		if (f.exists())
@@ -168,7 +151,7 @@ public class NetexStifLoadSharedDataTests extends Arquillian implements Constant
 		f.mkdirs();
 		jobData.setReferential("chouette_gui");
 		jobData.setAction(JobData.ACTION.importer);
-		jobData.setType( "netex_stif");
+		jobData.setType("netex_stif");
 		context.put(TESTNG, "true");
 		context.put(OPTIMIZED, Boolean.FALSE);
 		return context;
@@ -177,10 +160,10 @@ public class NetexStifLoadSharedDataTests extends Arquillian implements Constant
 
 	@Test(groups = { "LoadData" }, description = "check if shared data are loaded")
 	public void verifyCheckLoadData() throws Exception {
-		
+
 		Context context = initImportContext();
 		context.put(REFERENTIAL, new Referential());
-		Command command = CommandFactory.create(initialContext,NetexStifLoadSharedDataCommand.class.getName());
+		Command command = CommandFactory.create(initialContext, NetexStifLoadSharedDataCommand.class.getName());
 		try {
 			command.execute(context);
 		} catch (Exception ex) {
@@ -188,20 +171,17 @@ public class NetexStifLoadSharedDataTests extends Arquillian implements Constant
 			throw ex;
 		}
 		Referential referential = (Referential) context.get(REFERENTIAL);
-		Assert.assertEquals(referential.getSharedReadOnlyLines().size(), 1, "line size");
-		for (String key : referential.getSharedReadOnlyLines().keySet()) {
-			Assert.assertEquals(key,"STIF:CODIFLIGNE:Line:C00108","objectid should be good for lines");
-		}
-		Assert.assertEquals(referential.getSharedReadOnlyCompanies().size(), 1, "company size");
-		for (String key : referential.getSharedReadOnlyCompanies().keySet()) {
-			Assert.assertEquals(key,"STIF:CODIFLIGNE:Operator:011","objectid should be good for companies");
-		}
+		Assert.assertEquals(referential.getSharedReadOnlyLines().size(), 2, "line size");
+		Assert.assertNotNull(referential.getSharedReadOnlyLines().get("STIF:CODIFLIGNE:Line:C00108"),
+				"objectid STIF:CODIFLIGNE:Line:C00108 should be found for lines");
+		Assert.assertEquals(referential.getSharedReadOnlyCompanies().size(), 3, "company size");
+		Assert.assertNotNull(referential.getSharedReadOnlyCompanies().get("STIF:CODIFLIGNE:Operator:011"),
+				"objectid 011 should be found for companies");
 		Assert.assertEquals(referential.getSharedReadOnlyStopAreas().size(), 58, "stops size");
 		for (StopAreaLite stop : referential.getSharedReadOnlyStopAreas().values()) {
-			Assert.assertEquals(stop.getAreaType(),"zdep","stop type should be zdep");
+			Assert.assertEquals(stop.getAreaType(), "zdep", "stop type should be zdep");
 		}
 
 	}
-
 
 }
