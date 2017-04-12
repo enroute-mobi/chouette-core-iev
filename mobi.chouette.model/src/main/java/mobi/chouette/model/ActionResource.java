765947cf2af852d89756386f8d18348dfc93b94d
flabourot <flabourot@cityway.fr>
2017-04-12 08:44:23 +0200

    serial

--- mobi.chouette.model/src/main/java/mobi/chouette/model/ActionResource.java
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/ActionResource.java
@@ -6,6 +6,11 @@ import mobi.chouette.common.JobData;
 
 public abstract class ActionResource implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4385137259926047441L;
+
 	public abstract Long getId();
 	
 	public abstract JobData.ACTION getAction();
