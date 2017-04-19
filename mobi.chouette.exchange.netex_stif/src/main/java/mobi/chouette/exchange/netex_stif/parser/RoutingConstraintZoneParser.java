4de04bdc324712c0c22fbe0f3ae8d00d0b8bed8e
flabourot <flabourot@cityway.fr>
2017-04-19 15:08:14 +0200

    add schedulesStopPoint ref to retrieve stopPoint

--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/RoutingConstraintZoneParser.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/parser/RoutingConstraintZoneParser.java
@@ -1,5 +1,7 @@
 package mobi.chouette.exchange.netex_stif.parser;
 
+import java.util.List;
+
 import org.xmlpull.v1.XmlPullParser;
 
 import lombok.extern.log4j.Log4j;
@@ -8,6 +10,10 @@ import mobi.chouette.common.XPPUtil;
 import mobi.chouette.exchange.importer.Parser;
 import mobi.chouette.exchange.importer.ParserFactory;
 import mobi.chouette.exchange.netex_stif.Constant;
+import mobi.chouette.exchange.netex_stif.model.NetexStifObjectFactory;
+import mobi.chouette.model.RoutingConstraint;
+import mobi.chouette.model.StopPoint;
+import mobi.chouette.model.util.ObjectFactory;
 import mobi.chouette.model.util.Referential;
 
 @Log4j
@@ -17,20 +23,32 @@ public class RoutingConstraintZoneParser implements Parser, Constant {
 	public void parse(Context context) throws Exception {
 		XmlPullParser xpp = (XmlPullParser) context.get(PARSER);
 		Referential referential = (Referential) context.get(REFERENTIAL);
+		NetexStifObjectFactory factory = (NetexStifObjectFactory) context.get(NETEX_STIF_OBJECT_FACTORY);
 
 		String id = xpp.getAttributeValue(null, ID);
+		RoutingConstraint routingConstraint = ObjectFactory.getRoutingConstraint(referential, id);
 		while (xpp.nextTag() == XmlPullParser.START_TAG) {
 			if (xpp.getName().equals(NAME)) {
+				String name = xpp.getText();
+				routingConstraint.setName(name);
 				XPPUtil.skipSubTree(log, xpp);
 			} else if (xpp.getName().equals(MEMBERS)) {
 				while (xpp.nextTag() == XmlPullParser.START_TAG) {
 					if (xpp.getName().equals(SCHEDULED_STOP_POINT_REF)) {
+						String ref = xpp.getAttributeValue(null, ID);
+						List<StopPoint> list = factory.getStopPoints(ref);
+						List<StopPoint> routingList = routingConstraint.getStopPoints();
+						for (StopPoint stopPoint : list) {
+							if (!routingList.contains(stopPoint)) {
+								routingList.add(stopPoint);
+							}
+						}
 						XPPUtil.skipSubTree(log, xpp);
-					}else{
+					} else {
 						XPPUtil.skipSubTree(log, xpp);
 					}
 				}
-			}else{
+			} else {
 				XPPUtil.skipSubTree(log, xpp);
 			}
 		}
