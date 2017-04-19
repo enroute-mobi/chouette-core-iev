34a2d6257e4cc133349b905bc567fd02c28e82ad
flabourot <flabourot@cityway.fr>
2017-04-19 10:27:12 +0200

    creation

---
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/RoutingConstraint.java
@@ -0,0 +1,64 @@
+package mobi.chouette.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.annotations.Type;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "routing_constraint_zone")
+@NoArgsConstructor
+public class RoutingConstraint extends ChouetteIdentifiedObject {
+	
+	@Setter
+	@Getter
+	@Id
+	@Column(name = "id", nullable = false)
+	protected Long id;
+
+	/**
+	 * name
+	 * 
+	 * @return The actual value
+	 */
+	@Getter
+	@Column(name = "name")
+	private String name;
+
+	/**
+	 * set name <br/>
+	 * truncated to 255 characters if too long
+	 * 
+	 * @param value
+	 *            New value
+	 */
+	public void setName(String value) {
+		name = StringUtils.abbreviate(value, 255);
+	}
+		
+	@Getter
+	@Setter
+	@Column(name = "stop_point_ids",columnDefinition="bigint[]")
+	@Type(type = "mobi.chouette.model.usertype.LongArrayUserType")
+	private Long[] stopPointIds;
+	
+
+	@Getter
+	@Setter
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "route_id")
+	private Route route;
+	
+}
