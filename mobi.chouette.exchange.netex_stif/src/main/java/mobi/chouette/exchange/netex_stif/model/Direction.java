b06c3fa41a9fa56718d997f2a39e91aabcd5657f
Didier LALLEMAND <dlallemand@cityway.fr>
2017-07-05 19:34:19 +0200

    Direction control (in progress)

--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/model/Direction.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/model/Direction.java
@@ -4,19 +4,30 @@ import lombok.Getter;
 import lombok.Setter;
 import mobi.chouette.model.ChouetteIdentifiedObject;
 
-public class Direction extends ChouetteIdentifiedObject{
+public class Direction extends ChouetteIdentifiedObject {
 
 	/**
 	 * Direction Object
 	 */
 	private static final long serialVersionUID = -3171106219663785290L;
 
-	@Getter @Setter
+	@Getter
+	@Setter
 	private Long id;
-	
-	@Getter @Setter
+
+	@Getter
+	@Setter
 	private String name;
 
-	@Getter @Setter
+	@Getter
+	@Setter
 	private boolean filled = false;
+
+	@Getter
+	@Setter
+	private String directionType;
+
+	@Getter
+	@Setter
+	private String oppositeDirectionRef;
 }
--- mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/validator/DirectionValidator.java
+++ mobi.chouette.exchange.netex_stif/src/main/java/mobi/chouette/exchange/netex_stif/validator/DirectionValidator.java
@@ -1,14 +1,16 @@
 package mobi.chouette.exchange.netex_stif.validator;
 
+import mobi.chouette.common.Constant;
 import mobi.chouette.common.Context;
+import mobi.chouette.exchange.netex_stif.model.Direction;
+import mobi.chouette.exchange.validation.report.DataLocation;
 import mobi.chouette.exchange.validation.report.ValidationReporter;
 
 public class DirectionValidator extends AbstractValidator {
 
 	public static final String LOCAL_CONTEXT = DIRECTION;
-	
-	protected String getLocalContext()
-	{
+
+	protected String getLocalContext() {
 		return LOCAL_CONTEXT;
 	}
 
@@ -22,63 +24,95 @@ public class DirectionValidator extends AbstractValidator {
 		validationReporter.prepareCheckPointReport(context, L2_NeTExSTIF_Direction_2);
 	}
 
-	/** 
- 	 * <b>Titre</b> :[Netex] Contrôle de l'objet Direction : Name
- 	 * <p>
- 	 * <b>R&eacute;ference Redmine</b> : <a target="_blank" href="https://projects.af83.io/issues/2311">Cartes #2311</a>
- 	 * <p>
- 	 * <b>Code</b> : 2-NeTExSTIF-Direction-1
- 	 * <p>
- 	 * <b>Variables</b> :  néant
- 	 * <p>
- 	 * <b>Prérequis</b> :  néant
- 	 * <p>
- 	 * <b>Prédicat</b> :  L'objet Direction doit avoir son attribut Name renseigné.
- 	 * <p>
- 	 * <b>Message</b> :  {fichier}-Ligne {ligne}-Colonne {Colonne} : l'objet Direction d'identifiant {objectId} n'a pas de valeur pour l'attribut Name
- 	 * <p>
- 	 * <b>Criticité</b> :  error
- 	 * <p>
- 	 * 
- 	 *
- 	 * @param context
- 	 * @return
- 	 */
- 	public boolean check2NeTExSTIFDirection1(Context context, int lineNumber, int columnNumber) {
- 		// TODO : [STIF] @Didier Implementation Controle  2-NeTExSTIF-Direction-1 : [Netex] Contrôle de l'objet Direction : Name
- 		boolean result = true;
- 		return result;
- 	}
- 
- 
- 
- 	/** 
- 	 * <b>Titre</b> :[Netex] Contrôle de l'objet Direction : Attributs interdits
- 	 * <p>
- 	 * <b>R&eacute;ference Redmine</b> : <a target="_blank" href="https://projects.af83.io/issues/2312">Cartes #2312</a>
- 	 * <p>
- 	 * <b>Code</b> : 2-NeTExSTIF-Direction-2
- 	 * <p>
- 	 * <b>Variables</b> :  néant
- 	 * <p>
- 	 * <b>Prérequis</b> :  néant
- 	 * <p>
- 	 * <b>Prédicat</b> :  L'objet Direction doit avoir ses attributs DirectionType et OppositeDirectionRef non renseignés.
- 	 * <p>
- 	 * <b>Message</b> :  {fichier}-Ligne {ligne}-Colonne {Colonne} : l'objet Direction d'identifiant {objectId} définit un attribut {attribut interdit} non autorisé
- 	 * <p>
- 	 * <b>Criticité</b> :  error
- 	 * <p>
- 	 * 
- 	 *
- 	 * @param context
- 	 * @return
- 	 */
- 	public boolean check2NeTExSTIFDirection2(Context context, int lineNumber, int columnNumber) {
- 		// TODO : [STIF] @Didier Implementation Controle  2-NeTExSTIF-Direction-2 : [Netex] Contrôle de l'objet Direction : Attributs interdits
- 		boolean result = true;
- 		return result;
- 	}
- 
+	/**
+	 * <b>Titre</b> :[Netex] Contrôle de l'objet Direction : Name
+	 * <p>
+	 * <b>R&eacute;ference Redmine</b> : <a target="_blank" href="https://projects.af83.io/issues/2311">Cartes #2311</a>
+	 * <p>
+	 * <b>Code</b> : 2-NeTExSTIF-Direction-1
+	 * <p>
+	 * <b>Variables</b> : néant
+	 * <p>
+	 * <b>Prérequis</b> : néant
+	 * <p>
+	 * <b>Prédicat</b> : L'objet Direction doit avoir son attribut Name renseigné.
+	 * <p>
+	 * <b>Message</b> : {fichier}-Ligne {ligne}-Colonne {Colonne} : l'objet Direction d'identifiant {objectId} n'a pas
+	 * de valeur pour l'attribut Name
+	 * <p>
+	 * <b>Criticité</b> : error
+	 * <p>
+	 * 
+	 *
+	 * @param context
+	 * @return
+	 */
+	public boolean check2NeTExSTIFDirection1(Context context, Direction direction, int lineNumber, int columnNumber) {
+		// TODO : [STIF] @Didier=done Implementation Controle 2-NeTExSTIF-Direction-1 : [Netex] Contrôle de l'objet Direction
+		// : Name
+		boolean result = true;
+
+		if (direction.getName() == null) {
+			result = false;
+		}
+
+		if (!result) {
+			ValidationReporter validationReporter = ValidationReporter.Factory.getInstance();
+			String fileName = (String) context.get(Constant.FILE_NAME);
+			DataLocation location = new DataLocation(fileName, lineNumber, columnNumber, direction);
+			validationReporter.addCheckPointReportError(context, L2_NeTExSTIF_Direction_1, location);
+		}
+
+		return result;
+	}
+
+	/**
+	 * <b>Titre</b> :[Netex] Contrôle de l'objet Direction : Attributs interdits
+	 * <p>
+	 * <b>R&eacute;ference Redmine</b> : <a target="_blank" href="https://projects.af83.io/issues/2312">Cartes #2312</a>
+	 * <p>
+	 * <b>Code</b> : 2-NeTExSTIF-Direction-2
+	 * <p>
+	 * <b>Variables</b> : néant
+	 * <p>
+	 * <b>Prérequis</b> : néant
+	 * <p>
+	 * <b>Prédicat</b> : L'objet Direction doit avoir ses attributs DirectionType et OppositeDirectionRef non
+	 * renseignés.
+	 * <p>
+	 * <b>Message</b> : {fichier}-Ligne {ligne}-Colonne {Colonne} : l'objet Direction d'identifiant {objectId} définit
+	 * un attribut {attribut interdit} non autorisé
+	 * <p>
+	 * <b>Criticité</b> : error
+	 * <p>
+	 * 
+	 *
+	 * @param context
+	 * @return
+	 */
+	public boolean check2NeTExSTIFDirection2(Context context, Direction direction, int lineNumber, int columnNumber) {
+		// TODO : [STIF] @Didier=done Implementation Controle 2-NeTExSTIF-Direction-2 : [Netex] Contrôle de l'objet Direction
+		// : Attributs interdits
+		boolean result = true;
+
+		String forbiddenAttribute = null;
+		if (direction.getOppositeDirectionRef() != null) {
+			forbiddenAttribute = direction.getOppositeDirectionRef();
+			result = false;
+		} else if (direction.getDirectionType() != null) {
+			forbiddenAttribute = direction.getDirectionType();
+			result = false;
+		}
+
+		if (!result) {
+			ValidationReporter validationReporter = ValidationReporter.Factory.getInstance();
+			String fileName = (String) context.get(Constant.FILE_NAME);
+			DataLocation location = new DataLocation(fileName, lineNumber, columnNumber, direction);
+			validationReporter.addCheckPointReportError(context, L2_NeTExSTIF_Direction_2, location,
+					forbiddenAttribute);
+		}
+
+		return result;
+	}
 
 }
