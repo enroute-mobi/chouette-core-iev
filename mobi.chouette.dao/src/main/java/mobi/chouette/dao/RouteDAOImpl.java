a6d86474a6e5331e51204ab7b61f43739ded7ea6
Didier LALLEMAND <dlallemand@cityway.fr>
2017-08-22 15:13:37 +0200

    add RouteDAO.findByLineId

--- mobi.chouette.dao/src/main/java/mobi/chouette/dao/RouteDAOImpl.java
+++ mobi.chouette.dao/src/main/java/mobi/chouette/dao/RouteDAOImpl.java
@@ -1,13 +1,21 @@
 package mobi.chouette.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import mobi.chouette.model.Route;
+import mobi.chouette.model.Route_;
 
 @Stateless
-public class RouteDAOImpl extends GenericDAOImpl<Route> implements RouteDAO{
+public class RouteDAOImpl extends GenericDAOImpl<Route> implements RouteDAO {
 
 	public RouteDAOImpl() {
 		super(Route.class);
@@ -18,4 +26,17 @@ public class RouteDAOImpl extends GenericDAOImpl<Route> implements RouteDAO{
 		this.em = em;
 	}
 
+	public List<Route> findByLineId(final long lineId) {
+		// System.out.println("GenericDAOImpl.findByObjectId() : " + objectIds);
+		List<Route> result = null;
+
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Route> criteria = builder.createQuery(type);
+		Root<Route> root = criteria.from(type);
+		Predicate predicate = builder.in(root.get(Route_.lineId)).value(lineId);
+		criteria.where(predicate);
+		TypedQuery<Route> query = em.createQuery(criteria);
+		result = query.getResultList();
+		return result;
+	}
 }
