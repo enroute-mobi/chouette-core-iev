fdaf32b3dd612eebebdba379f4c0605cf8127031
Michel Etienne <metienne@cityway.fr>
2017-09-18 14:04:35 +0200

    comment end of range as excluded

--- mobi.chouette.model/src/main/java/mobi/chouette/model/type/DateRange.java
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/type/DateRange.java
@@ -13,19 +13,44 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
+/**
+ * Java model for Postgresql DateRange object
+ * 
+ * @author michel
+ * 
+ * @see mobi.chouette.model.usertype.DateRangeArrayUserType
+ * 
+ * @since 4.0
+ *
+ */
 @AllArgsConstructor
 @Log4j
 public class DateRange implements Serializable {
 	private static final long serialVersionUID = -4771648000594466515L;
+	/**
+	 * first date in range (included)
+	 * 
+	 * @return The first date in range (included)
+	 */
 	@Getter
 	@Setter
 	private Date first;
+	/**
+	 * last date in range (excluded)
+	 * 
+	 * @return The last date in range (excluded)
+	 */
 	@Getter
 	@Setter
 	private Date last;
 
+	/**
+	 * create a DateRange from Postgresql object
+	 * @param obj Postgresql object
+	 */
 	public DateRange(PGobject obj) {
 		if (obj.getType().equals("daterange")) {
+			// value starts with [ and ends with ) : last is excluded from range
 			String value = obj.getValue().substring(1, obj.getValue().length() - 1);
 			String[] tok = value.split(",");
 			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -42,12 +67,18 @@ public class DateRange implements Serializable {
 		}
 	}
 
+	/**
+	 * convert to Postgresql object for saving
+	 * 
+	 * @return Postgresql object instance
+	 * @throws SQLException
+	 */
 	public PGobject toPGObject() throws SQLException {
 		PGobject obj = new PGobject();
 		obj.setType("daterange");
-		SimpleDateFormat format1= new SimpleDateFormat("yyyy-MM-dd");
-		SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd");
-		obj.setValue("[" + format1.format(first) + "," + format2.format(last) + ")");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		// use parenthesis at end to set excluded value
+		obj.setValue("[" + format.format(first) + "," + format.format(last) + ")");
 		return obj;
 	}
 
