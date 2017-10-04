0a22ac5f88a0f807d99adfc233277918a0becde0
Michel Etienne <metienne@cityway.fr>
2017-10-04 09:41:21 +0200

    remore row_security

--- src/test/sql/80_public_insert_compliance-data-test.sql
+++ src/test/sql/80_public_insert_compliance-data-test.sql
@@ -10,8 +10,8 @@ SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
--- SET client_min_messages = warning;
-SET row_security = off;
+SET client_min_messages = warning;
+-- SET row_security = off;
 
 SET search_path = public, pg_catalog;
 
