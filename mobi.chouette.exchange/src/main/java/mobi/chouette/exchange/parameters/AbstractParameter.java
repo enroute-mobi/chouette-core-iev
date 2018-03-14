690ad5e492a841fd0f9313e518136b6a7dbb213a
Michel Etienne <metienne@cityway.fr>
2018-03-14 13:05:51 +0100

    #6136 manage several referential_metadata on import parameters

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/parameters/AbstractParameter.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/parameters/AbstractParameter.java
@@ -1,6 +1,7 @@
 package mobi.chouette.exchange.parameters;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
