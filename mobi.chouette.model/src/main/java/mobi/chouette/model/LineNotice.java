3bfaedc273213ab16fc2c5adc1263b73b0726f0c
Aurelien-af83 <aurelien.guiramand@gmail.com>
2019-05-23 11:33:38 +0200

    Model & dao

---
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/LineNotice.java
@@ -0,0 +1,96 @@
+package mobi.chouette.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.annotations.NaturalId;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name = "line_notices",schema="public")
+@Cacheable
+@NoArgsConstructor
+@EqualsAndHashCode(of = { "objectId" }, callSuper = false)
+@ToString(callSuper=true)
+public class LineNotice extends ChouetteIdentifiedObject {
+
+	private static final long serialVersionUID = -8086291270595894778L;
+
+	@Getter
+	@Setter
+	@SequenceGenerator(name="line_notices_id_seq", sequenceName="public.line_notices_id_seq", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="line_notices_id_seq")
+	@Id
+	@Column(name = "id", nullable = false)
+	protected Long id;
+
+
+	@Getter
+	@NaturalId(mutable=true)
+	@Column(name = "objectid", nullable = false, unique = true)
+	protected String objectId;
+
+	public void setObjectId(String value) {
+		objectId = StringUtils.abbreviate(value, 255);
+	}
+
+	@Getter
+	@Setter
+	@Column(name = "object_version")
+	protected Long objectVersion = 1L;
+
+	@Getter
+	@Column(name = "title")
+	private String title;
+
+	@Getter
+	@Setter
+	@Column(name="content",columnDefinition = "text")
+	private String content;
+
+	@Getter
+	@Setter
+	@Column(name="import_xml",columnDefinition = "text")
+	private String importXml;
+
+
+	/**
+	 * line referential reference
+	 *
+	 * @return The actual value
+	 */
+	@Getter
+	@Setter
+	@Column(name = "line_referential_id")
+	protected Long lineReferentialId;
+
+	// /**
+	//  * lines
+	//  *
+	//  * @param lines
+	//  *            New value
+	//  * @return The actual value
+	//  */
+	// @Getter
+	// @Setter
+	// @OneToMany(mappedBy = "company")
+	// private List<Line> lines = new ArrayList<>(0);
+
+}
